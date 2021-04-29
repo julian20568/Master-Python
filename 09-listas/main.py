37e67400c7fa59b5c9931f930de4ef4d1d110072
Julian Obando <jaop17@hotmail.com>
2021-04-29 18:44:49 -0500

    definir listas, indices  y añadir elementos a una lista

---
+++ 09-listas/main.py
@@ -0,0 +1,36 @@
+"""
+LISTAS (arrays)
+Son colecciones o conjuntos de datos/valores, bajo un unico
+nombre.
+Paa acceder a esos valores podemos usar un indice numerico
+"""
+
+pelicula = "Batman"
+#definir lista
+peliculas = ["Batman", "Spiderman", "La pasion de Cristo"]
+cantantes = list(('Barack', 'Alex Campos', 'Marcos Brunet', 'Miel San Marcos')) #debemos definir con tupla ()
+years = list(range(2020, 2050))
+variada = ["Julian", 10, 2.5, True, "Texto"]
+
+"""
+print(pelicula)
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)
+"""
+
+#Indices
+pelicula = "otra cosa"
+peliculas[1] = "Apocalipsis"
+print(peliculas)
+
+print(peliculas[1])#salida Spiderman
+print(peliculas[-2])#salida Spiderman
+print(cantantes[1:3])#sublista de rango de elementos que quiera ver
+print(peliculas[1:])#saca todos los elementos de la lista a partir de indice 1
+
+#Añadir elementos a una lista
+cantantes.append("Redimido2")
+cantantes.append("Julio Melgar")
+print(cantantes)
