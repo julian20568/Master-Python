791a446d5fc2d31f7ec2df05641a6107e59fa260
Julian Obando <jaop17@hotmail.com>
2021-04-29 10:22:42 -0500

    curiosidades de python

---
+++ 08-funciones/curiosidades.py
@@ -0,0 +1,17 @@
+def mi_funcion(nombre):
+    #print("Hola que tal")
+    return "Hola que tal" + nombre
+
+def mi_segunda_funcion(apellidos):
+    #print("Hola que tal 2")
+    return "Hola que tal 2" + apellidos
+
+nombre = "Julian"
+apellidos = "OBando"
+
+#print("hola mundo")
+#print("{} Bienvenido".format(nombre))
+#print(f"Bienvenido {nombre}")
+
+print(mi_funcion(nombre))
+print(mi_segunda_funcion(apellidos))
