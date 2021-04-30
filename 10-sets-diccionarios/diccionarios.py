1e33f7bb711d2a7e0d586c866380ae450c68b14d
Julian Obando <jaop17@hotmail.com>
2021-04-30 17:50:59 -0500

    Creando diccionarios en python

---
+++ 10-sets-diccionarios/diccionarios.py
@@ -0,0 +1,15 @@
+"""
+Diccionario:
+Es un tipo de datos que almacena un conjunto de datos.
+En formato clave > valor.
+Es parecido a un array asociativo o un objeto json
+"""
+
+persona = {
+    "nombre": "Julian",
+    "apellidos":"Obando",
+    "web":"julianobandoweb.es"
+}
+
+print(persona)
+print(persona["apellidos"])#acceder a un indice especifico
