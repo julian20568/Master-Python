dffcc7d8f1e90cfe0f785053c94812e0ff190332
Julian Obando <jaop17@hotmail.com>
2021-05-03 17:46:22 -0500

    commit ejercicio2

--- 11-ejercicios/ejercicio2.py
+++ 11-ejercicios/ejercicio2.py
@@ -3,3 +3,24 @@ Ejercicio2: Escribir un programa que añada valores a una lista
 mientras que su longitud sea menor a 120 y luego mostrar la lista
 Plus: Usar while y for
 """
+
+""" ##### bucle for ####
+coleccion = []
+
+for contador in range(0,120):
+    coleccion.append(f"elemento-{contador}")
+    print("Mostrando el: " + coleccion[contador])
+
+print(coleccion)
+
+"""
+
+coleccion = []
+
+x = 0
+while x < 120:
+    coleccion.append(f"elemento-{x}")
+    print("Mostrando el: " + coleccion[x])
+    x += 1
+
+print(coleccion)
