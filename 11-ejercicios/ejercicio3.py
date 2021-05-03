b6adff85e2bc70d3711123c897143e5da0f6d085
Julian Obando <jaop17@hotmail.com>
2021-05-03 17:52:08 -0500

    commit tercer ejercicio

--- 11-ejercicios/ejercicio3.py
+++ 11-ejercicios/ejercicio3.py
@@ -3,3 +3,12 @@ Ejercicio 3: Programa que compruebe si una variable esta vacia
 y si esta vacia, rellenarla con texto en minusculas y mostrarlo
 en mayusculas.
 """
+
+texto = ""
+
+if len(texto.strip()) <= 0:
+    #mostrar el texto
+    texto = "Hola soy un texto en minusculas"
+    print(texto.upper())
+else:
+    print(f"La variable tiene contenido {texto}")
