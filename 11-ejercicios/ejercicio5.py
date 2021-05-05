c4bdc8aa3101fc95f78dafcde0576f862429284a
Julian Obando <jaop17@hotmail.com>
2021-05-05 17:31:22 -0500

    commit del ejercicio 5

--- 11-ejercicios/ejercicio5.py
+++ 11-ejercicios/ejercicio5.py
@@ -5,3 +5,24 @@ GTA            ASSINS                FIFA 21
 COD            CRASH                 PRO 21
 PUGB           PRINCE OF PERSIA      MOTO GP 21
 """
+
+#crear un diccionario
+tabla = [
+    {
+        "categoria": "ACCION",
+        "juegos": ["GTA","COD","PUGB"]
+    },
+    {
+        "categoria": "AVENTURA",
+        "juegos": ["ASSINS","CRASH","PRINCE OF PERSIA"]
+    },
+    {
+        "categoria": "DEPORTES",
+        "juegos": ["FIFA 21", "PRO 21", "MOTO GP 21"]
+    }
+]
+
+for categoria in tabla:
+    print(f"--------------{categoria['categoria']}--------------")
+    for juegos in categoria['juegos']:
+        print(juegos)
