7c7b8839e6b8031391314187db43a4ee5ceb4d0a
Julián Obando <jaop17@hotmail.com>
2021-06-15 17:58:32 -0500

    commit para actualización de archivos

--- practicas.py
+++ practicas.py
@@ -1,15 +1,15 @@
-#Crear un programa que le permita al usuario imprimir
-#n tablas de multiplicar segun la cantidad que desee
+# Crear un programa que le permita al usuario imprimir
+# n tablas de multiplicar segun la cantidad que desee
+from builtins import print
 
 n = int(input("numero de tablas que desea imprimir? "))
 
-for cantidad in range(1,n):
+for cantidad in range(n, 1):
     numero = int(input("Ingrese la tabla {}: ".format(cantidad)))
 
-    for tabla in range(1,11):
+    for tabla in range(1, 11):
         print("{} X {} = {}".format(numero, tabla, numero * tabla))
 
 """
 
 """
-
