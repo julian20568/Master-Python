3ba9b6547f644f8788910a8643d075c855def3e9
Julián Obando <Julian@entrenese.com>
2021-12-28 18:20:36 -0500

    Commit de actualización

--- 07-ejercicios/Ejrcicio4.py
+++ 07-ejercicios/Ejrcicio4.py
@@ -1,19 +1,25 @@
+
+import numpy as np
 """
 Ejercicio4:
     Pedir dos numeros al usuario y hacer todas las 
     operaciones básicas de una calculadora y mostrarlo por pantalla.
 """
 
-print("###CALCULADORA###")
-num1 = int(input("Ingrese el primer número: "))
-num2 = int(input("Ingrese el segundo número: "))
+# print("###CALCULADORA###")
+# num1 = int(input("Ingrese el primer número: "))
+# num2 = int(input("Ingrese el segundo número: "))
 
-# suma = num1 + num2
-# resta = num1 - num2
-# multiplicacion = num1 * num2
-# division = num1 / num2
+# # suma = num1 + num2
+# # resta = num1 - num2
+# # multiplicacion = num1 * num2
+# # division = num1 / num2
 
-print("La suma es: {}".format(num1+num2))
-print("La resta es: {}".format(num1-num2))
-print("La multiplicación es: {}".format(num1*num2))
-print("La división es: {}".format(num1/num2))
+# print("La suma es: {}".format(num1+num2))
+# print("La resta es: {}".format(num1-num2))
+# print("La multiplicación es: {}".format(num1*num2))
+# print("La división es: {}".format(num1/num2))
+
+a = np.array([(1, 2, 3), (4, 5, 6)])
+a = a / 2
+print(a)
